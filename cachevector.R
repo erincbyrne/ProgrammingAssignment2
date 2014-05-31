2d5273ff1ab2cfce84d3310918a3b7757e61b6a0
erincbyrne <erincbyrne@gmail.com>
2014-05-31 19:40:38 -0400

    adapted cachevector to instead cache a matrix in cachematrix

--- cachevector.R
+++ cachevector.R
@@ -3,33 +3,33 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeVector <- function(x = numeric()) {
       m <- NULL
       set <- function(y) {
             x <<- y
             m <<- NULL
       }
       get <- function() x
-      setinverse <- function(inv) m <<- inv
-      getinverse <- function() m
+      setmean <- function(mean) m <<- mean
+      getmean <- function() m
       list(set = set, get = get,
-           setinverse = setinverse,
-           getinverse = getinverse)
+           setmean = setmean,
+           getmean = getmean)
 }
+
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-      m <- x$getinverse()
+cachemean <- function(x, ...) {
+      m <- x$getmean()
       if(!is.null(m)) {
             message("getting cached data")
             return(m)
       }
       data <- x$get()
-      m <- ginv(data, ...)
-      x$setinverse(m)
+      m <- mean(data, ...)
+      x$setmean(m)
       m
 }
 
